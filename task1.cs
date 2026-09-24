c81aab22900c77a9da92ed459164591ead0d63d1
Malika Amanbekova <k3632076@gmail.com>
2026-09-24 14:48:31 +0500

    Контрольная 1

---
+++ task1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+class ZooAnimal() 
+{
+    public void ZooAnimal()
+    {
+        private string _species {get; set; };
+        private double _weight {get; set; };
+
+    }
+ 
+    public virtual void sound()
+    {
+        Console.WriteLine("Животное издает звук");
+    }
+
+}
+
+class Lion : ZooAnimal
+public Lion(string _species, double _weight)
+{
+    public roar()
+    {
+        Console.WriteLine("Лев рычит");
+    }
+    public override void sound()
+    {
+        Console.WriteLine("Лев издает звук");
+    }
+};
+
+class Elephant:ZooAnimal
+{
+    public trumpet()
+    {
+        Console.WriteLine("Слон трубит");
+    }
+    public override void sound()
+    {
+        Console.WriteLine("Слон издает звук");
+    }
+}
+
+class Penguin:ZooAnimal
+{
+    public swim()
+    {
+        Console.WriteLine("Пингвин плавает");
+    }
+    public override void sound()
+    {
+        Console.WriteLine("Пингвин издает звук");
+    }
+};
